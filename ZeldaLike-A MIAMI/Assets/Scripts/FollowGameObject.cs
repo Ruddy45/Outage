01ccc0956d031494375340a2c13ae53105972c1b
Wesley Petit <ptitwes.45@gmail.com>
2020-09-11 18:31:21 +0200

    Push du projet original
    Commentaire en français

---
+++ ZeldaLike-A MIAMI/Assets/Scripts/FollowGameObject.cs
@@ -0,0 +1,22 @@
+﻿/* Author : Raphaël Marczak - 2018/2020, for MIAMI Teaching (IUT Tarbes)
+ * 
+ * This work is licensed under the CC0 License. 
+ * 
+ */
+
+using UnityEngine;
+
+// Permet à la camera de suivre le joueur
+public class FollowGameObject : MonoBehaviour
+{
+	public GameObject objectToFollow;                                  // Objet à suivre, ici le joueur
+
+	// A chaque frame, la camera se place sur le joueur en x et y
+	// le z reste celui définie sur la camera
+	void Update()
+	{
+		transform.position = new Vector3(objectToFollow.transform.position.x,
+											  objectToFollow.transform.position.y,
+											  transform.position.z);
+	}
+}
---
+++ ZeldaLike-A MIAMI/Assets/Scripts/PositionRegardingPlayer.cs
@@ -0,0 +1,45 @@
+﻿/* Author : Raphaël Marczak - 2018/2020, for MIAMI Teaching (IUT Tarbes)
+ * 
+ * This work is licensed under the CC0 License. 
+ * 
+ */
+
+using UnityEngine;
+
+// Cette classe est utilisé pour être sûre qu'un sprite de NPC va apparaître derrière le joueur
+// quand le joueur est devant lui, et appaître devant quand le joueur est derrière lui
+public class PositionRegardingPlayer : MonoBehaviour
+{
+	SpriteRenderer npcRenderer;                                          // Sprite du NPC
+
+	void Start()
+	{
+		// Récupére le composant sur le gameObject
+		npcRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	void Update()
+	{
+		// Charche le joueur
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+		// Si un joueur existe
+		if (player != null)
+		{
+			// Récupèrer son sprite renderer et déterminer sa position par rapport au NPC
+			SpriteRenderer playerRenderer = player.GetComponent<SpriteRenderer>();
+
+			if (playerRenderer != null)
+			{
+				if (transform.position.y < player.transform.position.y)
+				{
+					npcRenderer.sortingOrder = playerRenderer.sortingOrder + 1;
+				}
+				else
+				{
+					npcRenderer.sortingOrder = playerRenderer.sortingOrder - 1;
+				}
+			}
+		}
+	}
+}
