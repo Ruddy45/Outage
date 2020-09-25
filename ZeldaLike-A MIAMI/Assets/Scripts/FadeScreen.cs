a19c6727057ec6847977d8f438f22d93000d61a1
Wesley Petit <ptitwes.45@gmail.com>
2020-09-25 19:22:02 +0200

    Introduction et Fade Screen

---
+++ ZeldaLike-A MIAMI/Assets/Scripts/FadeScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FadeScreen : MonoBehaviour
+{
+	[SerializeField] private DialogManager _dialogManager = null;
+	[SerializeField] private Animation _animation = null;
+
+	private void Awake() => _dialogManager.OnEndDialogue += Fade;
+
+	private void Fade()
+	{
+		_animation.Play();
+		_dialogManager.OnEndDialogue -= Fade;
+		StartCoroutine(Destroy());
+	}
+
+	private IEnumerator Destroy()
+	{
+		yield return new WaitForSecondsRealtime(_animation.clip.length);
+		Destroy(gameObject);
+	}
+}
